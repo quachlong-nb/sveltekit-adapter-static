c01e23d3dd305c034a0a594ed4480f8e85487a95
longqv <longqv@bfd.vn>
2023-11-12 18:10:23 +0700

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert color="green">
+		<span class="font-medium">Success alert!</span>
+		Change a few things up and try submitting again.
+	</Alert>
+</div>
