0f52e6b16f6964929108f3959dde0b4d9992943d
longqv <longqv@bfd.vn>
2023-11-13 00:31:19 +0700

    config capacitor

---
+++ android/app/src/main/java/com/sveltekitapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sveltekitapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sveltekitapp',
+  appName: 'ß╗Éi dß╗ôi ├┤i',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
